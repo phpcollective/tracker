55894cc46f543649b439fee41ca78e954f34271f
Al Amin Ibne Mosaddeque <chayan.php@gmail.com>
2019-04-15 10:44:44 +0600

    first commit

---
+++ src/TrackingServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PhpCollective\Tracker;
+
+use Illuminate\Support\ServiceProvider;
+
+class TrackingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
